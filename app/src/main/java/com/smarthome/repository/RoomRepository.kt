90c74eaf821051a6925468f7dd2409ebe91d3c9b
Sunny <sunnytse0326@gmail.com>
2018-09-12 12:14:53 +0800

    Add fixture control api in repository

--- app/src/main/java/com/smarthome/repository/RoomRepository.kt
+++ app/src/main/java/com/smarthome/repository/RoomRepository.kt
@@ -4,6 +4,7 @@ import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.rx.rx_object
 import com.github.kittinunf.result.Result
+import com.smarthome.model.FixtureControl
 import com.smarthome.model.Rooms
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -12,7 +13,14 @@ import io.reactivex.schedulers.Schedulers
 class RoomRepository: BaseRepository() {
     companion object {
         val FETCH_ROOM = "rooms"
+        enum class RoomType(val url: String) {
+            BEDROOM("bedroom"),
+            LIVING_ROOM("living-room"),
+            KITCHEN("kitchen")
+        }
     }
 
     fun fetchRooms(): Single<Result<Rooms, FuelError>>? = Fuel.get("/$FETCH_ROOM").rx_object(Rooms.Deserializer()).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread())
+
+    fun controlFixture(url: String): Single<Result<FixtureControl, FuelError>>? = Fuel.get(url).rx_object(FixtureControl.Deserializer()).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread())
 }
