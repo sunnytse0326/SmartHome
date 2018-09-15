18d22e2c07ddf7f54cfd3e067b61f089ba81e882
Sunny <sunnytse0326@gmail.com>
2018-09-15 16:51:19 +0800

    Disable application class usage

--- app/src/main/java/com/smarthome/utils/NetworkCache.kt
+++ app/src/main/java/com/smarthome/utils/NetworkCache.kt
@@ -1,19 +1,20 @@
 package com.smarthome.utils
 
+import android.content.Context
 import com.smarthome.model.Cache
 import com.smarthome.repository.BaseRepository
 import java.util.*
 
-class NetworkCache {
-    companion object {
-        fun get(key: String): Object? {
-            val data: Cache? = if (Memory.getObjectData(key) != null) (Memory.getObjectData(key) as Cache) else Cache(null,0)
-            return if(Date().time - (data?.time?:0) < BaseRepository.CACHETIME) data?.data else null
-        }
+class NetworkCache(mContext: Context) {
+    private val context: Context = mContext
 
-        fun set(key: String, data: Object) {
-            val cache = Cache(data, Date().time)
-            Memory.saveObjectData(key, cache)
-        }
+    fun get(key: String): Object? {
+        val data: Cache? = if (Memory(context).getObjectData(key) != null) (Memory(context).getObjectData(key) as Cache) else Cache(null, 0)
+        return if (Date().time - (data?.time ?: 0) < BaseRepository.CACHETIME) data?.data else null
+    }
+
+    fun set(key: String, data: Object) {
+        val cache = Cache(data, Date().time)
+        Memory(context).saveObjectData(key, cache)
     }
 }
